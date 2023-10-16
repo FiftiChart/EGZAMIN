b0f4f52935df90cd681e27bac4b152408639a01f
MaciejG <43651871+FiftiChart@users.noreply.github.com>
2023-10-16 16:56:27 +0200

    skończona stronka

--- PRAKTYKA/arkEE.09-2022-01-01/zapisz.php
+++ PRAKTYKA/arkEE.09-2022-01-01/zapisz.php
@@ -3,9 +3,8 @@ $c = mysqli_connect('localhost', 'root', '', 'wedkowanie');
 $imie = $_POST["imie"];
 $nazwisko = $_POST["nazwisko"];
 $adres = $_POST["adres"];
-$q = mysqli_query($c, "INSERT INTO karty_wedkarskie(imie, nazwisko, adres) VALUES($imie, $nazwisko, $adres);");
+$q = mysqli_query($c, "INSERT INTO karty_wedkarskie(imie, nazwisko, adres) VALUES ('$imie', '$nazwisko', '$adres');");
 
 
 
 mysqli_close($c);
-?>
