a6f9517bcdc770d1cde00c7517c445991ebb7b74
MaciejG <43651871+FiftiChart@users.noreply.github.com>
2023-10-25 18:36:27 +0200

    podstawa

--- PRAKTYKA/ark.INF.09-2022-01-02/pogoda.php
+++ PRAKTYKA/ark.INF.09-2022-01-02/pogoda.php
@@ -1,6 +1,34 @@
-<?php
-// Zrobić skrypt dwiema pętlami
-// pobieranie rekordów - zewnętrzną
-// wpisywanie <td> - wewnętrzną (foreach)
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prognoza pogody Wrocław</title>
+    <link rel="stylesheet" href="styl2.css">
+</head>
+<body>
+    <div class="baner_lewy"><img src="./logo.png" alt="meteo"></div>
+    <div class="baner_srodkowy"><h1>Prognoza Wrocławia</h1></div>
+    <div class="baner_prawy"><p>maj, 2019r.</p></div>
+    <div class="glowny">
+        <table>
+            <tr>
+                <th>DATA</th>
+                <th>TEMPERATURA W NOCY</th>
+                <th>TEMPERATURA W DZIEŃ</th>
+                <th>OPADY [mm/h]</th>
+                <th>CIŚNIENIE [hPa]</th>
+            </tr>
+            <?php
+            // Zrobić skrypt dwiema pętlami
+            // pobieranie rekordów - zewnętrzną
+            // wpisywanie <td> - wewnętrzną (foreach)
 
-?>
+            ?>
+        </table>
+    </div>
+    <div class="lewy"><img src="./obraz.jpg" alt="Polska, Wrocław"></div>
+    <div class="prawy"><a href="./kwerendy.txt">Pobierz kwerendy</a></div>
+    <div class="stopka"><p>Stronę wykonał: PESEL</p></div>
+</body>
+</html>
