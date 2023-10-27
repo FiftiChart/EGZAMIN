43d0e20968373ac2b07bd3e42f89af77e7c042ab
FiftiChart <maciej3b@gmail.com>
2023-10-27 10:06:49 +0200

    zajecia 2710

--- PRAKTYKA/ark.INF.09-2022-01-02/pogoda.php
+++ PRAKTYKA/ark.INF.09-2022-01-02/pogoda.php
@@ -1,6 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prognoza pogody Wrocław</title>
+    <link rel="stylesheet" href="styl2.css">
+</head>
+<body>
+
 <?php
 // Zrobić skrypt dwiema pętlami
 // pobieranie rekordów - zewnętrzną
 // wpisywanie <td> - wewnętrzną (foreach)
 
 ?>
+</body>
+</html>
+
