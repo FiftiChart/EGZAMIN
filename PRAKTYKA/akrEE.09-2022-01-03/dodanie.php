096d635b479051d584a72f2e36f16c0f3a4fb7f3
MaciejG <43651871+FiftiChart@users.noreply.github.com>
2023-10-29 17:08:05 +0100

    działanie php

--- PRAKTYKA/akrEE.09-2022-01-03/dodanie.php
+++ PRAKTYKA/akrEE.09-2022-01-03/dodanie.php
@@ -3,11 +3,8 @@
     $ratownik_1 = $_POST['ratownik_1'];
     $ratownik_2 = $_POST['ratownik_2'];
     $ratownik_3 = $_POST['ratownik_3'];
-    print($ratownik_1);
-    print($ratownik_2);
-    print($ratownik_3);
     $c = mysqli_connect('localhost','root','','ee09');
-    $q = mysqli_query($c,"INSERT INTO ratownicy (nrKaretki, ratownik1, ratownik2, ratownik3) VALUES ($nr_karetki, `$ratownik_1`, `$ratownik_2`, `$ratownik_3`)");
-    echo("Do bazy zostało wysłane zapytanie: $q");
+    $q = mysqli_query($c,"INSERT INTO ratownicy (nrKaretki, ratownik1, ratownik2, ratownik3) VALUES ($nr_karetki, '$ratownik_1', '$ratownik_2', '$ratownik_3')");
+    echo("Do bazy zostało wysłane zapytanie: INSERT INTO ratownicy (nrKaretki, ratownik1, ratownik2, ratownik3) VALUES ($nr_karetki, '$ratownik_1', '$ratownik_2', '$ratownik_3');");
     mysqli_close($c);
 ?>
